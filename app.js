1c49cb37a9eb78410201c97eac541261067bca15
Facundo Cancelo <facundo.cancelo@gmail.com>
2022-07-15 17:46:01 -0300

    second commit

---
+++ app.js
@@ -0,0 +1,20 @@
+require('colors');
+
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+console.clear(); 
+
+const main = async() => {
+    console.log('Hola Mundo');
+
+    let opt = '';
+    
+    do {
+        opt = await mostrarMenu();
+        console.log( { opt } );
+        await pausa();
+    } while (opt !== '0' );
+
+}
+
+main();
